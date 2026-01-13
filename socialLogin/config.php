6e8b28ae2239230e927dcc6def6790301483c1e7
Afzal Ahmed <afzal_209@hotmail.com>
2026-01-14 00:07:50 +0500

    dynamic url config file

--- socialLogin/config.php
+++ socialLogin/config.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'google-api/vendor/autoload.php';
+$actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 // session_start();
 $clientID = '424279592981-hts1a7l2vm2nholmikl2lv2n4ah4h595.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'https://safasolo.com/login.php';
+$redirectUri = $actual_link.'/login.php';
 
 // Initialize the $client object
 $client = new Google_Client();
