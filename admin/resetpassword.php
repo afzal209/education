fa70d5473069b512e9552c5a0975631ff8430bc9
Afzal Ahmed <afzal_209@hotmail.com>
2026-01-15 09:53:42 +0500

    update code in reset file

--- admin/resetpassword.php
+++ admin/resetpassword.php
@@ -4,6 +4,41 @@
         
     $email=$_GET['email'];
     $token=$_GET['token'];
+
+									   if (isset($_POST['submit'])) {
+                                        include 'db/connect.php';
+                                        $email=$_POST['email'];
+                                        $token=$_POST['token'];
+                                    
+                                    
+                                        
+                                        $new=$_POST['newpassword'];
+                                        $confirm=$_POST['confirmpassword'];
+                                    
+                                        $hash = password_hash($new ,PASSWORD_DEFAULT);
+                                    
+                                         if (password_verify($confirm,$hash)) {
+                                            //  echo 'equal';
+                                            //$hash = password_hash($new,PASSWORD_BCRYPT);
+                                            $query=mysqli_query($con,"update user set password='$hash',token='' where email='$email'");
+                                            // echo $query;
+                                            if ($query) {
+                                                //echo $query;
+                                                header(' location: login.php?response=success&class=success&message=Password Change Successfully!');
+                                            } else {
+                                                //echo $query;
+                                    
+                                                header('location:forgetpassword.php?response=error&class=danger&message=Kindly forgot Password Again');
+                                            }
+                                            
+                                         } 
+                                        //  else {
+                                        //      header('location: ../resetpassword.php?token='.$token.'&email='.$email.'&response=error&class=danger&message=Password not Match!');
+                                        //  }
+                                    
+                                    } 
+                                    
+
     $select=mysqli_query($con,"select id from user where email='$email' and token='$token'");
     if(mysqli_num_rows($select) > 0){
         include_once 'includeFile/header.php'; 
@@ -53,40 +88,7 @@
                                         <!-- <button class="genric-btn success-border circle arrow">Login<span class="lnr lnr-arrow-right"></span></button> -->
                                     </div>                          
                                 </form>
-                                <?php
-									   if (isset($_POST['submit'])) {
-                                        include 'db/connect.php';
-                                        $email=$_POST['email'];
-                                        $token=$_POST['token'];
-                                    
-                                    
-                                        
-                                        $new=$_POST['newpassword'];
-                                        $confirm=$_POST['confirmpassword'];
-                                    
-                                        $hash = password_hash($new ,PASSWORD_DEFAULT);
-                                    
-                                         if (password_verify($confirm,$hash)) {
-                                            //  echo 'equal';
-                                            //$hash = password_hash($new,PASSWORD_BCRYPT);
-                                            $query=mysqli_query($con,"update user set password='$hash',token='' where email='$email'");
-                                            // echo $query;
-                                            if ($query) {
-                                                //echo $query;
-                                                header(' location: login.php?response=success&class=success&message=Password Change Successfully!');
-                                            } else {
-                                                //echo $query;
-                                    
-                                                header('location:forgetpassword.php?response=error&class=danger&message=Kindly forgot Password Again');
-                                            }
-                                            
-                                         } 
-                                        //  else {
-                                        //      header('location: ../resetpassword.php?token='.$token.'&email='.$email.'&response=error&class=danger&message=Password not Match!');
-                                        //  }
-                                    
-                                    } 
-                                    ?>
+                                
                             </div>
                         </div>
                     </div>
