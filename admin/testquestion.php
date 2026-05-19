172d83db258fd0f8268b74bcc391f83aa79103bb
Afzal Ahmed <afzal_209@hotmail.com>
2026-05-19 22:04:39 +0500

    refactor(testquestion.php): improve file structure, enhance form handling, and update UI components for adding test questions

--- admin/testquestion.php
+++ admin/testquestion.php
@@ -1,5 +1,7 @@
 <?php 
-        include_once 'db/connect.php';
+           // include_once 'db/connect.php';
+        require_once dirname(__DIR__) .'/config.php'; 
+        include(BASE_PATH.'db/connect.php');
         if(!isset($_SESSION['user']['email']))
         {
             header('location:login.php');
@@ -10,98 +12,151 @@
 
 
         <?php
-        include_once 'includeFile/header.php'; 
-        ch_title("Add Test Question");
-        include_once 'includeFile/navbar.php';
+         include_once(BASE_PATH .'/includes/header.php'); 
+	ch_title("Moalym", "Add Test Question");
         ?>
-            <section class="banner-area relative" id="home">	
-                    <div class="overlay overlay-bg"></div>
-                    <div class="container">				
-                        <div class="row d-flex align-items-center justify-content-center">
-                            <div class="about-content col-lg-12">
-                                <h1 class="text-white">
-                                    Add Test Question 				
-                                </h1>	
-                                <!-- <p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span><a href="blog-home.html">Blog </a> <span class="lnr lnr-arrow-right"></span> <a href="blog-single.html"> Blog Details Page</a></p> -->
-                            </div>	
-                        </div>
-                    </div>
-                </section>
+             <div id="wrapper">
 
-                <div class="whole-wrap">
-                    <div class="container" >
-                        <div class="section-top-border">
-                            <div class="row">
-                                <div class="col-lg-8 col-md-8">
-                                    <h3 class="mb-30 text-center">Add Question</h3>
+    <!-- Sidebar -->
+    <?php 
+    include_once(BASE_PATH .'/includes/sidebar.php');
+    ?>
+
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            <?php 
+        include_once(BASE_PATH .'/includes/topbar.php')
+        ?>
+
+
+            <main id="main" class="main">
+                <div class="container" style="margin: auto;">
+                    <div class="row ">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h2>Add Test Question</h2>
+                                </div>
+                                <div class="card-body">
                                     <?php 
                                         if(@$_GET['response'] != ''){
                                             echo '  <div class="alert alert-'.@$_GET['class'].'">
                                                         <strong>'.ucfirst(@$_GET['response']).'!</strong> '.@$_GET['message'].'
                                                     </div>';
                                                 }
+
+                                                  $timezone = "Asia/Karachi";
+                                            date_default_timezone_set($timezone);
+                                           $today = date("Y-m-d");
                                     ?>
-                                    <form  method="POST" action="phpScript/test_question_script.php" >
-                                        <div class="form-group mt-10">
-                                                <select class="form-control" name="subject" id="subject" >
-                                                    <option value="">Subject Name</option>
-                                                <?php
-                                                $query = mysqli_query($con,"select * from test_subject");
-                                                if(mysqli_num_rows($query) > 0){
-                                                    while($row=mysqli_fetch_assoc($query)){
-                                                    echo '<option value="'.$row['id'].'">'.$row['subject_name'].'</option>';
-                                                    }
-                                                }    
-                                                else{
-                                                    echo '<option value="">No Subject Added</option>';
-                                                }
+                                    <form method="POST" action="<?php echo BASE_URL; ?>admin/phpScript/test_question_script.php"
+                                        enctype="multipart/form-data">
+                                        <input type="hidden" name="insert_by" value="<?php echo $_SESSION['user']['id']; ?>">
+                                        <div class="mb-3">
+                                            <label for="role" class="form-label">Subject</label>
+                                            <select class="form-control" name="subject" id="subject" >
+                                                <option value="" selected>subject Name</option>
+                                                    <?php
+                                                    $query=mysqli_query($con,"select * from test_subject where status_post =2 ");
+                                                    while ($row=mysqli_fetch_assoc($query)) { 
+                                                    ?>
+                                                    <option value="<?php echo $row['id'];?>"><?php echo $row['subject_name'];?></option>
+                                                    <?php 
+                                                        }
                                                 ?>
-                                                </select>
+                                            </select>
+                                        </div> 
+                                        <div class="mb-3">
+                                            <label for="role" class="form-label">Chapter</label>
+                                            <select class="form-control" name="chapter" id="chapter" >
+                                                <option value="" selected>Chapter Name</option>
+                                                   
+                                            </select>
+                                        </div>   
+
+                                        <div class="mb-3">
+                                            <label for="role" class="form-label">Topic</label>
+                                            <select class="form-control" name="topic" id="topic" >
+                                                <option value="" selected>Topic Name</option>
+                                                   
+                                            </select>
                                         </div>
-                                        <div class="form-group mt-10">
-                                                <select class="form-control" name="chapter" id="chapter" >
-                                                    <option value="">Chapter Name</option>
-                                                </select>
+
+                                        <div class="form-floating mb-3">
+                                            <textarea type="text" class="form-control" id="question" name="question"
+                                                placeholder="Enter Question" required></textarea>
+                                            <label for="question">Enter Question</label>
                                         </div>
-                                        <div class="form-group mt-10">
-                                                <select class="form-control" name="topic" id="topic" >
-                                                    <option value="">Topic Name</option>
-                                                </select>
+
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="option1" name="option1"
+                                                placeholder="Enter Option 1" required>
+                                            <label for="option1">Enter Option 1</label>
                                         </div>
-                                        <div class="mt-10">
-                                            <textarea name="question" id="question" class="single-textarea" placeholder="Enter Article" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Article'" required></textarea>
+
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="option2" name="option2"
+                                                placeholder="Enter Option 2" required>
+                                            <label for="option2">Enter Option 2</label>
                                         </div>
-                                        <div class="mt-10">
-                                            <input type="text" name="option1" id="option1" placeholder="Enter Option 1" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Option 1'" required class="single-input">
+
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="option3" name="option3"
+                                                placeholder="Enter Option 3" required>
+                                            <label for="option3">Enter Option 3</label>
                                         </div>
-                                        <div class="mt-10">
-                                            <input type="text" name="option2" id="option2" placeholder="Enter Option 2" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Option 2'" required class="single-input">
+                                              
+
+                                        <div class="form-floating mb-3">
+                                            <input type="text" class="form-control" id="option4" name="option4"
+                                                placeholder="Enter Option 4" required>
+                                            <label for="option4">Enter Option 4</label>
                                         </div>
-                                        <div class="mt-10">
-                                            <input type="text" name="option3" id="option3" placeholder="Enter Option 3" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Option 3'" required class="single-input">
-                                        </div>
-                                        <div class="mt-10">
-                                            <input type="text" name="option4" id="option4" placeholder="Enter Option 4" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Option 4'" required class="single-input">
-                                        </div>
-                                        <div class="form-group mt-10">
-                                            <select class="form-control" name="correct" id="correct">
-                                                <option value="" selected>Correct Answer</option>
+
+                                        
+                                        
+                                        <div class="mb-3">
+                                            <label for="role" class="form-label">language</label>
+                                            <select class="form-control" name="correct" id="correct" >
+                                                <option value="" selected>Select Correct</option>
                                                 <option value="option1">Option 1</option>
                                                 <option value="option2">Option 2</option>
                                                 <option value="option3">Option 3</option>
                                                 <option value="option4">Option 4</option>
+                                                
+                                                <!-- Add more language options as needed -->
                                             </select>
-                                    </div>
-                                        <input type="hidden" name="insert_by" value="<?php echo @$_SESSION['user']['username']; ?>" >             
-                                        <div class="button-group-area mt-40">
-                                            <input class="genric-btn success-border circle" type="submit" name="submit" value="Add">
-                                        </div>                                   
+                                        </div>
+
+                                        <!-- Assign Academic -->
+                                        
+                                        <!-- Submit -->
+                                        <div class="col-12 mb-3">
+                                            <input type="submit" class="btn btn-primary" name="submit" value="Add">
+                                        </div>
+
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+
+            </main><!-- End #main -->
+
+        
+            <!-- Footer -->
+            <?php 
+       include(BASE_PATH .'/includes/copy_write.php')
+       ?>
+        </div>
+    </div>
+</div>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
                 <script type="text/javascript">
                 $(document).ready(function(e){
@@ -142,6 +197,9 @@
                 });
             </script>   
 
-        <?php
-        include('includeFile/footer.php');
-        ?>
+       <?php
+    // include('includeFile/footer.php');
+
+     include_once(BASE_PATH.'/includes/footer.php'); 
+    
+    ?>
