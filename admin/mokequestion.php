d2cb91c4a2bf4297928f7f7e22ea8f49ff7ca7ba
Afzal Ahmed <afzal_209@hotmail.com>
2026-05-23 15:40:55 +0500

    refactor(mokequestion.php): enhance structure by including configuration, updating header and navigation, and improving breadcrumb display

--- admin/mokequestion.php
+++ admin/mokequestion.php
@@ -1,98 +1,256 @@
 <?php 
-        include_once 'db/connect.php';
-        if(!isset($_SESSION['user']['email']))
-        {
-            header('location:login.php');
-        }
-        include_once 'includeFile/header.php'; 
-        ch_title("Moke Question");
-        include_once 'includeFile/navbar.php';
+          require_once dirname(__DIR__) .'/config.php';
+// echo BASE_PATH;
+// exit;
+
+
+    // include(BASE_PATH.'db/connect.php');
+
+     include(BASE_PATH.'db/connect.php');
+
+    if(!isset($_SESSION['user']['email']))
+    {
+        header('location:index.php');
+    }
         ?>
-        <section class="banner-area relative" id="home">	
-                    <div class="overlay overlay-bg"></div>
-                    <div class="container">				
-                        <div class="row d-flex align-items-center justify-content-center">
-                            <div class="about-content col-lg-12">
-                                <h1 class="text-white">
-                                    Moke Question				
-                                </h1>	
-                                <!-- <p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span><a href="blog-home.html">Blog </a> <span class="lnr lnr-arrow-right"></span> <a href="blog-single.html"> Blog Details Page</a></p> -->
-                            </div>	
-                        </div>
-                    </div>
-                </section>
-                <section class="post-content-area single-post-area">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-8 posts-list">
-							<div class="single-post row">
-								<!-- <div class="col-lg-12">
-									<div class="feature-img">
-										<img class="img-fluid" src="img/blog/feature-img1.jpg" alt="">
-									</div>									
-                                </div> -->
-                                <div class="col-lg-12 col-md-12 col-sm-12">
-                                <?php
-                                $id=$_GET['id'];
+
+         <?php
+   include_once(BASE_PATH .'/includes/header.php'); 
+	ch_title("Moalym", "Make Question");
+    ?>
+          <div id="wrapper">
+
+
+
+    <!-- Sidebar -->
+
+    <?php 
+
+    include(BASE_PATH .'includes/sidebar.php');
+
+    ?>
+
+
+
+    <div id="content-wrapper" class="d-flex flex-column">
+
+
+
+        <!-- Main Content -->
+
+        <div id="content">
+
+
+
+            <!-- Topbar -->
+
+            <?php 
+
+        include(BASE_PATH .'includes/topbar.php');
+$id=$_GET['id'];
                                 $query=mysqli_query($con,"select test_subject.* , test_chapter.* ,test_topic.*  from test_subject inner join test_chapter on test_subject.id = test_chapter.test_subject_id inner join test_topic on test_chapter.id = test_topic.test_chapter_id where test_topic.id='$id'");
                                 $row=mysqli_fetch_assoc($query); 
-                                echo'
-                                    <h1 class="text-center" style="font-family: Arial, Helvetica, sans-serif;font-size:30px">Moke Test</h1>
-                                    <h1 style="font-family: Arial, Helvetica, sans-serif;font-size:30px"><a href="mokeacademic.php">Test Subject</a>>'.$row['subject_name'].'><a href="mokesubject.php?id='.$row['test_subject_id'].'">'.$row['chapter_name'].'</a>><a href="mokechapter.php?id='.$row['test_chapter_id'].'">'.$row['topic_name'].'</a></h1>
-                                ';     
-                                ?>    
-                                    
-                            
+        ?>
+
+
+
+<div class="container-fluid mt-3">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb bg-light p-2 rounded">
+
+            <!-- <li class="breadcrumb-item">
+                <a href="dashboard.php">Dashboard</a>
+            </li> -->
+
+            <li class="breadcrumb-item">
+                Moke Test
+            </li>
+
+            <li class="breadcrumb-item" >
+                <a href="mokeacademic.php">Test Subject</a>
+            </li>
+
+              <li class="breadcrumb-item" >
+                <a href="mokesubject.php?id=<?php echo $row['test_subject_id']; ?>"><?php echo $row['subject_name']; ?></a>
+                
+            </li>
+
+
+              <li class="breadcrumb-item">
+                <a href="mokechapter.php?id=<?php echo $row['test_chapter_id']; ?>"><?php echo $row['chapter_name']; ?></a>
+
+               
+                
+            </li>
+
+             <li class="breadcrumb-item active" aria-current="page">
+                <?php echo $row['topic_name']; ?>
+                
+            </li>
+               
+
+        </ol>
+    </nav>
+</div>
+
+
+
+
+
+            <main id="main" class="main">
+
+                <div class="container" style="margin: auto;">
+
+                    <div class="row ">
+
+                        <div class="col-12">
+
+                            <div class="card">
+
+                                <div class="card-header">
+
+                                    <h2>Moke Topic</h2>
+
                                 </div>
-                                <div class="col-md-6 col-sm-6">
-                                    <form method="POST" action="phpScript/moke_question_script.php">
-                                        <ol class="cs">
-                                            <?php
-                                            if(@$_GET['response'] != ''){
-                                            echo ' <div class="alert alert-'.@$_GET['class'].'">
+
+                                <div class="card-body">
+<?php 
+                                        if(@$_GET['response'] != ''){
+                                            echo '  <div class="alert alert-'.@$_GET['class'].'">
                                                         <strong>'.ucfirst(@$_GET['response']).'!</strong> '.@$_GET['message'].'
                                                     </div>';
-                                            }
-                                            echo '
-                                                <h1 class="text-center">Question</h1>
-                                                <div class="col-md-12 col-sm-12 form-group">
-                                                <select class="form-control" name="job_title" id="job_title">
-                                                    <option></option>';
-                                                $query1=mysqli_query($con,"select * from moke_title");
-                                                if (mysqli_num_rows($query1) > 0) {
-                                                    while($row1=mysqli_fetch_assoc($query1)){
-                                                    echo'<option value="'.$row1['id'].'">'.$row1['job_title'].'</option>';
+                                                }
+                                    ?>
+                                    <form method="POST" action="<?php echo BASE_URL; ?>admin/phpScript/moke_question_script.php"
+                                        enctype="multipart/form-data">
+                                        <input type="hidden" name="insert_by" value="<?php echo $_SESSION['user']['id']; ?>">
+                                        <div class="mb-3">
+                                            <label for="role" class="form-label">Organization Name</label>
+                                            <select class="form-control" name="job_title" id="job_title" required>
+                                                <option value="">Select Moke</option>
+                                                    <?php
+                                                    $query=mysqli_query($con,"select * from moke_title");
+                                                    if (mysqli_num_rows($query) > 0) {
+                                                    while ($row=mysqli_fetch_assoc($query)) { 
+                                                    ?>
+                                                    <option value="<?php echo $row['id'];?>"><?php echo $row['job_title'];?></option>
+                                                    <?php 
                                                         }
                                                     }
-                                                else {
+                                                    else{
                                                     header("location: addmoke.php?response=error&class=danger&message=First Add moke Test");
                                                     }
-                                                echo '
-                                                </select>
-                                                </div>';
+                                                ?>
+                                            </select>
+                                        </div>    
+
+                                        <div class="col-md-12">
+
+                                       <div class="table-wrap">
+
+                                            <table class="table table-responsive-lg table-striped-columns">
+
+                                                <thead style="background-color: green;">
+
+                                                    <tr>
+
+                                                       
+
+                                                       
+
+                                                        <th class="thed" scope="col">#</th>
+
+                                                        <th scope="col">Topic Name</th>
+                                                        
+                                                        
+                                                        
+                                                        
+
+                                                    </tr>
+
+                                                </thead>
+
+                                                <tbody>
+
+                                                    <?php 
+                                                    $id=$_GET['id'];
+                                                $a = 1;
+                                                $query=mysqli_query($con,"select * from test_question where test_topic_id='$id' and status_post = 2");
+                                                    if(mysqli_num_rows($query) > 0){
+                                                      while($row=mysqli_fetch_assoc($query)){ 
+                                                       
+                                                        echo '<tr>'
+
+                                                        .'<td><input type="checkbox"  name="checkbox[]" value="'.$row['id'].'"> </td>'
+                                                        .'<input type="hidden" name="t_id" value="'.$row['test_topic_id'].'"/>'
+                                                         .'<td>'.$row['question'].'</td>'.
+                                                        
+                                                        '</tr>';
+                                                      }
+                                                      echo '<input type="hidden" name="time"  id="time"/>';
+                                                    }
+                                                    // print_r($view_subject);  
+
+                                                   
+
                                                     
-                                                $query=mysqli_query($con,"select * from test_question where test_topic_id='$id'");
-                                                while($row=mysqli_fetch_assoc($query)){
-                                                echo '
-                                                    <div class="col-md-12 col-sm-12 form-group">
-                                                        <li>
-                                                            <input type="checkbox"  name="checkbox[]" value="'.$row['id'].'"> <h1 style="margin-left:4%; margin-top:-8%;" name="question">'.$row['question'].'</h1>
-                                                            <input type="hidden" name="t_id" value="'.$row['test_topic_id'].'"/>
-                                                        </li>
-                                                    </div>    ';
-                                                        } 
-                                                        echo '<input type="hidden" name="time"  id="time"/>';
+
+                                                    // print_r(view_subject($con,'academic'));
+
+                                                    
+
                                                     ?>
-                                        </ol> 
-                                         <input type="hidden" name="insert_by" value="<?php echo @$_SESSION['user']['username']; ?>" >
-                                        <input type="submit" class="btn btn-primary" style="margin-left: 5%;"  name="t_submit" value="Add" >
+
+                                                </tbody>
+
+                                            </table>
+
+                                        </div>
+
+
+
+                                    </div>
+                                        
+                                        <!-- Submit -->
+                                        <div class="col-12 mb-3">
+                                            <input type="submit" class="btn btn-primary" name="t_submit" value="Add">
+                                        </div>
+
                                     </form>
+
                                 </div>
-							</div>	
-						</div>
-					</div>
-				</div>	
-            </section>
+
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+
+
+            </main><!-- End #main -->
+
+            
+
+            <!-- Footer -->
+
+            <?php 
+
+       include(BASE_PATH .'includes/copy_write.php')
+
+       ?>
+
+        </div>
+
+    </div>
+
+</div>
             
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -119,5 +277,6 @@
                 });
             </script>
 <?php
-        include('includeFile/footer.php');
-        ?>
+         include_once(BASE_PATH.'/includes/footer.php'); 
+
+    ?>
