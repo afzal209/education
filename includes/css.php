d8d693f4e6c8cb8b777ae8f883eab94bff0fa8b0
Afzal Ahmed <afzal_209@hotmail.com>
2026-06-03 01:38:45 +0500

    refactor(css.php): standardize link tag formatting for stylesheet inclusion

--- includes/css.php
+++ includes/css.php
@@ -1,5 +1,7 @@
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
+
 
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
    
