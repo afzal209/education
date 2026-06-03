181c73bc24977b9a56f06e374272ff4cd1ab26b0
Afzal Ahmed <afzal_209@hotmail.com>
2026-06-04 00:55:13 +0500

    refactor(addquestion.php): add hidden input for user role in question form

--- admin/addquestion.php
+++ admin/addquestion.php
@@ -82,6 +82,7 @@
                                     <form method="POST" action="<?php echo BASE_URL; ?>admin/phpScript/question_script.php"
                                         enctype="multipart/form-data">
                                         <input type="hidden" name="insert_by" value="<?php echo $_SESSION['user']['id']; ?>">
+<input type="hidden" name="role" value="<?=$_SESSION['user']['role']?>">
                                         
                                         <!-- Username -->
                                         <div class="mb-3">
