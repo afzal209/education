7ab40140c90127b1e5a772140dd673f6387c0ecc
Afzal Ahmed <afzal_209@hotmail.com>
2026-06-25 18:28:12 +0500

    refactor(query.php): update academic query to include status_show filter

--- function/query.php
+++ function/query.php
@@ -11,7 +11,7 @@ function view_academic($con, $type = NULL)
         return $value;
     } else {
 
-        $query = "select * from academic where insert_type= '$type' and status_post =2";
+        $query = "select * from academic where insert_type= '$type' and status_post =2 and status_show =1";
         // echo $query;
         // exit;
         $my_query = mysqli_query($con, $query);
