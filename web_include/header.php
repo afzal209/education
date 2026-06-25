6cd7860397bee05516f1766ea2a4940f87628bcc
Afzal Ahmed <afzal_209@hotmail.com>
2026-06-25 21:02:21 +0500

    refactor(header.php): remove unnecessary blank line in the head section

--- web_include/header.php
+++ web_include/header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
     <link href="web_asset/css/style.css" rel="stylesheet" />
     <link href="web_asset/css/responsive.css" rel="stylesheet" />
+    
 
 <style>
 #search_bar {
