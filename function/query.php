fb7fbdc295d5c123cf0361c2a4d9c19f5c8f32ab
Afzal Ahmed <afzal_209@hotmail.com>
2026-06-25 20:02:41 +0500

    refactor(query.php): update view_subject query to include status_show filter

--- function/query.php
+++ function/query.php
@@ -37,7 +37,7 @@ function view_subject($con, $type = NULL, $id = NULL)
         return $value;
     } else {
 
-        $query = "select subj.*,aca.academic_name from academic aca,subject subj where aca.id = subj.academy_id and subj.status_post =2 and subj.insert_type = '$type' and subj.academy_id='$id'  ";
+        $query = "select subj.*,aca.academic_name from academic aca,subject subj where aca.id = subj.academy_id and subj.status_post =2 and subj.status_show =1 and subj.insert_type = '$type' and subj.academy_id='$id'  ";
 
         $my_query = mysqli_query($con, $query);
         $value = array();
