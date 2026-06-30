1c2e09ce16af327d2eeee161058aaa82508a69b8
Afzal Ahmed <afzal_209@hotmail.com>
2026-07-01 02:16:59 +0500

    refactor(subjectdelete.php): streamline deletion process by updating related entities in a single transaction

--- admin/phpDeleteScript/subjectdelete.php
+++ admin/phpDeleteScript/subjectdelete.php
@@ -1,39 +1,52 @@
 <?php
 include('../db/connect.php');
-$id=$_GET['id'];
+$subject_id=$_GET['id'];
 $delete_at = date('Y-m-d H:i:s');
-$delete = mysqli_query($con,"delete from questions_meta where question_id = '$id'");
-if ($delete) {
-    $delete_question=mysqli_query($con,"delete from question where topic_id = '$id'");
-    if($delete_question){
-        $delete_topic = mysqli_query($con,"delete from topic where chapter_id ='$id' ");
-        if ($delete_topic) {
-            $delete_chapter=mysqli_query($con,"delete from chapter where id = '$id'");
-            if($delete_chapter){
-                $delete_subject=mysqli_query($con,"update subject set status_show = 2, deleted_at = '$delete_at' where id = '$id'");
-                if($delete_subject){
-                    header('location:../viewsubject.php');
-                }
-                else{
-                    echo 'error in delete subject query';
-                }
-            }
-            else{
-                echo 'error in delete chapter query';
-            }
-        }
-        else{
-            echo 'error in delete topic query';
-        }    
-    }
-    else{
-        echo 'error in delete question query';
-    }
-}
-else{
-    echo 'error in delete  query';
 
+// Update Questions
+$q2 = mysqli_query($con, "
+UPDATE question q
+JOIN topic t ON q.topic_id = t.id
+JOIN chapter c ON t.chapter_id = c.id
+SET q.status_show = 2,
+    q.deleted_at = '$delete_at'
+WHERE c.subject_id = '$subject_id'
+");
+
+// Update Topics
+$q3 = mysqli_query($con, "
+UPDATE topic t
+JOIN chapter c ON t.chapter_id = c.id
+SET t.status_show = 2,
+    t.deleted_at = '$delete_at'
+WHERE c.subject_id = '$subject_id'
+");
+
+// Update Chapters
+$q4 = mysqli_query($con, "
+UPDATE chapter
+SET status_show = 2,
+    deleted_at = '$delete_at'
+WHERE subject_id = '$subject_id'
+");
+
+// Update Subject
+$q5 = mysqli_query($con, "
+UPDATE subject
+SET status_show = 2,
+    deleted_at = '$delete_at'
+WHERE id = '$subject_id'
+");
+
+if ($q1 && $q2 && $q3 && $q4 && $q5) {
+    mysqli_commit($con);
+    header("Location: ../viewsubject.php");
+    exit;
+} else {
+    mysqli_rollback($con);
+    echo "Error: " . mysqli_error($con);
 }
+
 
 
 ?>
